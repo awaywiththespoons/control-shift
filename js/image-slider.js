72cc8fd14a3aba70d3ebc4b1dd6e181282c0c1f5
EllieForeman <foreman.eleanor@gmail.com>
2021-01-07 18:31:36 +0000

    removed unused code and commented image-slide.js

--- js/image-slider.js
+++ js/image-slider.js
@@ -1,57 +1,64 @@
+window.onresize = changeButtonPosition; //because the image container is set as absolute, we need a way of telling if the image is smaller on some screens to prevent a gap between the projects div and the navigation buttons
+
+function changeButtonPosition() {
+    projectContainerHeight = document.querySelector('.project-image').clientHeight
+    document.querySelector('.projects').style.height = (projectContainerHeight + 15)+ 'px'
+}
+
 function init(){
-    gsap.set('.project', {x: '-100%'});
+    changeButtonPosition()
+    gsap.set('.project', {x: '-100%'}); // images start off screen
     gsap.set('.project', {autoAlpha: 1});
+
     let currentStep = 0; //first image
     const totalSlides = document.querySelectorAll('.project').length //total images
     const wrapper = gsap.utils.wrap(0, totalSlides) 
-    createfirstTimeline('next', currentStep)
 
-    function createfirstTimeline(direction, index) {
+    createfirstTimeline(currentStep)
+
+    function createfirstTimeline(index) {
+        leftImage = index -1 < 0 ? totalSlides -1 : index - 1; // if user presses back it will show last image
         const imageInnerRight = document.querySelector('div.project0' + index)
         const imageFurthestRight = document.querySelector('div.project0' + (index+1));
-        leftImage = index -1 < 0 ? totalSlides -1 : index - 1;
         const imageInnerLeft = document.querySelector('div.project0' + leftImage);
-        const timelineIn = gsap.timeline({
-            id: "timelineIn"
-        });
+        const timelineIn = gsap.timeline({ id: "timelineIn" }); //creating gsap timeline
         timelineIn.fromTo(imageInnerRight, {
-            x: '100%', //starting position (set above)
+            x: '100%', //starting position - entering from righthand side
             }, { 
                 duration: 1.3,
-                x: 0,
+                x: 0, //ending position - moving into the center
             }
         ),
         timelineIn.fromTo(imageInnerLeft, {
-            x: '-100%', //starting position (set above)
+            x: '-100%', //starting position - entering from the lefthand side
             }, { 
                 duration: 1.0,
-                x: -650,
-            }, 0.7
+                x: -650, //ending position - moving into the far left position (partially off lefthand side)
+            }, 0.7 // this sets a 0.7 second delay in tween starting
         ),
         timelineIn.fromTo(imageFurthestRight, {
             // autoAlpha: 0, //autoAlpha animates opacity - in this case from 0 to 1 
             x: '100%', //starting position (set above)
             }, { 
                 duration: 1.0,
-                x: 650,
-            }, 0.7 // this 0 sets this tween to start at time 0
+                x: 650, //ending position - moving into the far right position (partially off right side)
+            }, 0.7 // this sets a 0.7 second delay in tween starting
         )
         return timelineIn;
     }
 
     function createTimelineIn(direction, index, current) {
-        console.log('index', index)
-        console.log('current', current)
         const goPrev = direction === 'prev';
-        let imageFurthestLeft, imageInnerLeft, imageFurthestRight, imageInnerRight;
-        progressBar = document.querySelector('.gallery-list__progress-bar')
-        progressBarWidth = document.querySelector('.gallery-list__progress-track').clientWidth
-        const progressIndicatorWidth = progressBar.clientWidth
-        let position;
-        let overflow;
+        let imageFurthestLeft, imageInnerLeft, imageFurthestRight, imageInnerRight, position, overflow;
+        // creating progress bar 
+        progressBar = document.querySelector('.gallery-list__progress-bar') //to target in order to change position of progressbar indicator
+        const progressIndicatorWidth = progressBar.clientWidth //width of progressbar indicator
+        progressBarWidth = document.querySelector('.gallery-list__progress-track').clientWidth //working out width of whole progress bar (based on 100% width of page)
+        console.log('progressBarWidth', progressBarWidth)
+        console.log('progressIndicatorWidth', progressIndicatorWidth)
         if (current === 0 && direction === 'prev') {
             position = progressBarWidth - progressIndicatorWidth
-            overflow = true;
+            overflow = true; //not using at the moment, but I was going to use the overflow variable, indicating whether the user is going from first -> last or last -> first image in order to change look of animation (so it doesn't whizz across whole bar) 
         } else if(current === totalSlides -1 && direction === 'next') {
             position = 0
             overflow = true;
@@ -60,15 +67,14 @@ function init(){
         } else {
             position = `+=` + (progressBarWidth - progressIndicatorWidth)/(totalSlides - 1);
         }
-        console.log('progress', position)
-        if (!goPrev) {
+        if (!goPrev) { //working out which images to target in order to change their position in the timeline below
             toExit = current -1 < 0 ? totalSlides -1 : current - 1;
             toEnter = index + 1 > totalSlides - 1 ? 0 : index + 1;
             imageFurthestLeft = document.querySelector('div.project0' + toExit);
             imageInnerRight = document.querySelector('div.project0' + index);
             imageFurthestRight = document.querySelector('div.project0' + toEnter);
             imageInnerLeft = document.querySelector('div.project0' + current);
-        } else {
+        } else { //working out which images to target in order to change their position in the timeline below
             toEnter = index -1 < 0 ? totalSlides -1 : index - 1;
             toExit = current + 1 > totalSlides - 1 ? 0 : current + 1;
             imageFurthestLeft = document.querySelector('div.project0' + toEnter);
@@ -80,7 +86,7 @@ function init(){
         timelineIn.fromTo(imageFurthestRight, {
             x: 1300, 
             }, { 
-                runBackwards: goPrev ? true : false,
+                runBackwards: goPrev ? true : false, //if user pressed back (goPrev is true) then runBackwards will reverse the start and end x position
                 duration: 0.7,
                 x: 650,
             } 
@@ -119,34 +125,22 @@ function init(){
 
     function updateCurrentStep(goToIndex) {
         currentStep = goToIndex;
-        //set active to the right dot
-        document.querySelectorAll('.dot').forEach(function(imageInnerRight, index) {
-            imageInnerRight.setAttribute('class', 'dot')
-            if(index === currentStep) {
-                imageInnerRight.classList.add('active')
-            }  
-        })
-        positionDot()
     }
 
     function transition(direction, toIndex) {
-        // MASTER TIMELINE that calls both timelines (In and then out). I could add a delay with .add(timelineOut, '+=2')
         const timelineTransition = gsap.timeline({
             onStart: function() {
                 console.log({fromIndex: currentStep}, {toIndex});
                 updateCurrentStep(toIndex)
             }
         })
-        const tlIn = createTimelineIn(direction, toIndex, currentStep)
-
-        timelineTransition
-            .add(tlIn) // then bring in the next slide
-        
+        const tlIn = createTimelineIn(direction, toIndex, currentStep) 
+        timelineTransition.add(tlIn) //calls function above createTimelineIn
         return timelineTransition;
     }
 
     function isTweening() {
-        return gsap.isTweening('.project');
+        return gsap.isTweening('.project'); //prevents user being able to click twice on arrows and flick through many images before the transition is finished
     }
 
     document.querySelector('button.next').addEventListener('click', function(e) {
@@ -160,63 +154,6 @@ function init(){
         const prevStep = wrapper(currentStep-1)
         !isTweening() && transition('prev', prevStep)
     })
-
-    function createNavigation() {
-        //create dots container
-        const newDiv = document.createElement('div');
-        newDiv.setAttribute('class', 'dots');
-
-        //making dot animation
-        const spot = document.createElement('div')
-        spot.setAttribute('class', 'spot')
-
-        //create a dot for each slide
-        for (let index = 0; index < totalSlides; index ++) {
-            const imageInnerRight = document.createElement('button')
-            const text = document.createTextNode(index);
-            imageInnerRight.appendChild(text);
-            imageInnerRight.setAttribute('class','dot');
-            if(currentStep === index) {
-                imageInnerRight.classList.add('active')
-            }
-            imageInnerRight.addEventListener('click', function() {
-                if (!isTweening() && currentStep !== index) {
-                    const direction = index > currentStep ? 'next' : 'prev'
-                    transition(direction, index)
-                }
-            })
-            newDiv.appendChild(imageInnerRight)
-        }
-        //add to projects container
-        newDiv.appendChild(spot)
-        document.querySelector('.projects').appendChild(newDiv)
-        positionDot();
-    }
-
-    function positionDot() {
-        const activeDotX = document.querySelector('.dot.active').offsetLeft;
-        const spot = document.querySelector('.spot')
-        const spotX = spot.offsetLeft
-        const destinationX = Math.round(activeDotX - spotX + 5);
-
-        const dotTL = gsap.timeline();
-        dotTL
-            .to(spot, {
-                duration: 0.4,
-                x: destinationX,
-                scale: 2.5,
-                ease: 'power1.Out'
-            })
-            .to(spot, {
-                    duration: 1.2, 
-                    scale: 1, 
-                    ease: 'power1.in'
-                }
-            )
-    }
-
-    createNavigation();
-
 }
 
 window.addEventListener('load', function(){
